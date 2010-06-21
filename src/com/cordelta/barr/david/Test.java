7beb0d4489052354b4ee50ab307dd8fbe1dbf801
David Barr <david.barr@cordelta.com>
2010-06-21 17:06:59 +1000

    Test graft and insert efficiency.

--- src/com/cordelta/barr/david/Test.java
+++ src/com/cordelta/barr/david/Test.java
@@ -1,9 +1,8 @@
 package com.cordelta.barr.david;
 
-import java.io.File;
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.Random;
 
 public class Test {
 
@@ -14,53 +13,59 @@ public class Test {
     public static void main(String[] args) {
         Node root = null;
 
-        LinkedList<File> q = new LinkedList<File>();
-        q.offer(new File("/Users/david/Documents/svn-dump-fast-export"));
-        while (!q.isEmpty()) {
-            File f = q.poll();
-            File[] files = f.listFiles();
-            if (files != null) q.addAll(Arrays.asList(files));
-            root = Node.insert(root, null, iterator(f.getPath()));
-        }
+//        LinkedList<File> q = new LinkedList<File>();
+//        q.offer(new File("/Users/david/Documents/svn-dump-fast-export"));
+//        while (!q.isEmpty()) {
+//            File f = q.poll();
+//            File[] files = f.listFiles();
+//            if (files != null) q.addAll(Arrays.asList(files));
+//            root = Node.insert(root, null, iterator(f.getPath()));
+//        }
+//
+//        root = Node.graft(root, null, iterator("/Users/david/Documents/svn-dump-fast-export.old"),
+//                Node.search(root, iterator("/Users/david/Documents/svn-dump-fast-export")));
+//
+//        root = Node.graft(root, null, iterator("/Users/david/Documents/svn-dump-fast-export/refs"),
+//                Node.search(root, iterator("/Users/david/Documents/svn-dump-fast-export/.git/refs")));
+//        root = Node.remove(root, null, iterator("/Users/david/Documents/svn-dump-fast-export/.git"));
+//
+//        Node.diff("",
+//                Node.search(root, iterator("/Users/david/Documents/svn-dump-fast-export.old")).middle,
+//                Node.search(root, iterator("/Users/david/Documents/svn-dump-fast-export")).middle,
+//                new Node.DeltaHandler() {
+//
+//                    public void delta(String path, Node a, Node b) {
+//                        if (a == null) {
+//                            System.out.println("A " + path);
+//                        } else if (b == null) {
+//                            System.out.println("D " + path);
+//                        } else {
+//                            System.out.println("R " + path);
+//                        }
+//                    }
+//                });
 
-        root = Node.graft(root, null, iterator("/Users/david/Documents/svn-dump-fast-export.old"),
-                Node.search(root, iterator("/Users/david/Documents/svn-dump-fast-export")));
-
-        root = Node.graft(root, null, iterator("/Users/david/Documents/svn-dump-fast-export/refs"),
-                Node.search(root, iterator("/Users/david/Documents/svn-dump-fast-export/.git/refs")));
-        root = Node.remove(root, null, iterator("/Users/david/Documents/svn-dump-fast-export/.git"));
-
-        Node.diff("",
-                Node.search(root, iterator("/Users/david/Documents/svn-dump-fast-export.old")).middle,
-                Node.search(root, iterator("/Users/david/Documents/svn-dump-fast-export")).middle,
-                new Node.DeltaHandler() {
-
-                    public void delta(String path, Node a, Node b) {
-                        if (a == null) {
-                            System.out.println("A " + path);
-                        } else if (b == null) {
-                            System.out.println("D " + path);
-                        } else {
-                            System.out.println("R " + path);
-                        }
-                    }
-                });
-
+        Random r = new Random();
         for (int i = 0; i < 10000; ++i) {
+            root = Node.insert(root, null, iterator("/" + (i / 100) + "/" + r.nextInt()));
+            if (i % 100 == 99) {
+                root = Node.graft(root, null, iterator("/" + (i / 100 + 1)),
+                        Node.search(root, iterator("/" + (i / 100))));
+            }
         }
 
         System.gc();
 
         System.out.println("Number of Nodes: " + Node.count());
 
-        q.offer(new File("/Users/david/Documents/svn-dump-fast-export"));
-        while (!q.isEmpty()) {
-            File f = q.poll();
-            File[] files = f.listFiles();
-            if (files != null) q.addAll(Arrays.asList(files));
-            System.out.println(Node.search(root, iterator(f.getPath())) != null);
-        }
+//        q.offer(new File("/Users/david/Documents/svn-dump-fast-export"));
+//        while (!q.isEmpty()) {
+//            File f = q.poll();
+//            File[] files = f.listFiles();
+//            if (files != null) q.addAll(Arrays.asList(files));
+//            System.out.println(Node.search(root, iterator(f.getPath())) != null);
+//        }
 
-        System.out.println(root);
+//        System.out.println(root);
     }
 }
